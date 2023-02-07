0c1ce19a95955eb0b509367f298c78702c4316ca
amr hanix <aabdelrazek@csss.es>
2023-02-07 17:03:21 +0200

    first commit

---
+++ input.py
@@ -0,0 +1,8 @@
+length=int(input("please enter length: "))
+width=int(input("please enter width: "))
+
+if length >0 and width >0:
+    area=int(length)*int(width)
+    print("the area of your rectangle is: ",area)
+else:
+    print("unvalid numbers")    
